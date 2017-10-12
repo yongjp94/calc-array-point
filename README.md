8a14a3b038dde3e8208a9433d441b2bd57875853
Yongjun Park <yongjp94@gmail.com>
2017-10-12 16:17:27 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# calc-array-point
---
+++ calc-array-points/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  calc-array-points
+//
+//  Created by iGuest on 10/12/17.
+//  Copyright © 2017 yongjp94. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
